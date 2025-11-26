6e3d8fa97de844cff537a373b172b42c89a4615a
souravk29 <sk2661901@gmail.com>
2025-11-26 23:59:32 +0530

    updated

--- app/src/main/java/com/example/hilt_dependency_injection/MainActivity.kt
+++ app/src/main/java/com/example/hilt_dependency_injection/MainActivity.kt
@@ -9,8 +9,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.hilt_dependency_injection.ui.theme.Hiltdependency_injectionTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +22,20 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
 
+            helloWish()
+
 
         }
     }
 }
 
+@Composable
+fun helloWish(){
+    Text(
+        text = "Hello",
+        modifier = Modifier.fillMaxSize(),
+        color = Color.Magenta,
+        30.sp,
+
+        )
+}
