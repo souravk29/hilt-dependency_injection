3cb7ee0d84f70982eb9b4581bee840d39da1f0c6
souravk29 <sk2661901@gmail.com>
2025-11-30 13:04:31 +0530

    added dependencies for hilt

--- app/src/main/java/com/example/hilt_dependency_injection/Car.kt
+++ app/src/main/java/com/example/hilt_dependency_injection/Car.kt
@@ -7,7 +7,7 @@ class Car( private val engine: Engine ) {
     dependency is created in class => for every other class we will have to create
     dependency (val engine) to use it, its not most reliable way
 
-    private val engine = Engine()      -> it's a dependency, "engine" depends on "Engine()"
+    private val engine = Engine()          -> it's a dependency, "engine" depends on "Engine()"
 
     */
 
--- app/src/main/java/com/example/hilt_dependency_injection/MainActivity.kt
+++ app/src/main/java/com/example/hilt_dependency_injection/MainActivity.kt
@@ -10,3 +10,32 @@ fun main(){
 
 
 }
+
+
+
+/*
+
+Dependency Injection:
+
+   the most reliable way is to create dependencies
+   externally and then feed it to the classes.
+
+
+
+Hilt:
+
+   A DI library built on Dagger that simplifies the dependency
+   injection in android projects.
+
+
+
+Component:
+
+    A container that knows how to provide dependencies ( singleton component, activity component )
+
+
+Module:
+
+    A class that provides dependencies using @Module and @Provides
+
+*/
