1876c6c1a8780030ec050519f5ccfde1faf0773d
souravk29 <sk2661901@gmail.com>
2025-12-03 00:02:52 +0530

    updated

--- app/src/main/java/com/example/hilt_dependency_injection/AppComponent.kt
+++ app/src/main/java/com/example/hilt_dependency_injection/AppComponent.kt
@@ -4,11 +4,21 @@ import dagger.Component
 import javax.inject.Singleton
 
 
+
+/*
+
+This snippet is a definition of a Dagger Component. In the context of Android development (and Kotlin/Java in general),
+Dagger is a Dependency Injection (DI) framework, and the Component is the most critical piece of the puzzle.
+
+*/
+
+
 @Singleton
 @Component(modules = [AppModule::class])
 interface AppComponent {
 
-    fun getCar(): Car                        // this interface defines the api for retrieving the dependencies from the dagger dep graph
+    fun getCar(): Car                        // this interface defines the api for retrieving the dependencies from the dagger dependency graph
+
 
 }
 
@@ -17,5 +27,20 @@ interface AppComponent {
 /*
 
  @Component : it is like a bridge between the modules (which provides the dependencies) & the classes that need those dependencies
+
+*/
+
+/*
+
+@Component(modules = [AppModule::class])  :
+
+This is the heart of the definition.
+
+The Annotation: By marking the interface with @Component, you are telling Dagger: "Please generate code that implements this interface for me."
+
+The Modules: modules = [AppModule::class] tells Dagger where to look for "recipes" or instructions on how to create objects.
+
+Dagger will look inside AppModule to find functions annotated with @Provides to figure out how to build the dependencies needed for the graph.
+
 
 */
--- app/src/main/java/com/example/hilt_dependency_injection/MainActivity.kt
+++ app/src/main/java/com/example/hilt_dependency_injection/MainActivity.kt
@@ -3,16 +3,21 @@ package com.example.hilt_dependency_injection
 fun main(){
 
 
-    /*
-
-    Hilt/Dagger  generates the DaggerAppComponent class at  compile-time
-
-    */
-
+//    Hilt/Dagger  generates the DaggerAppComponent class at  compile-time
 
 
    val appComponent: AppComponent =
             DaggerAppComponent.create()                         // generates java folder in app
+
+
+    // retrieve the car instance
+
+    val car: Car = appComponent.getCar()
+
+
+    // use the car
+
+    println(car.drive())
 
 }
 
